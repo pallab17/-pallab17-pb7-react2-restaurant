869e3c0e6b9412689e6ca3d01459a2504d405b0a
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-22 00:33:45 +0530

    files updated

--- pb7react2/src/components/basics/Restaurant.js
+++ pb7react2/src/components/basics/Restaurant.js
@@ -1,28 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./style.css";
+import Menu from "./menuApi";
 
 
  const Restaurant = () => {
+    const [menuData,setMenuData] = useState(Menu);
+    console.log(menuData);
+
   return (
     <>
-     <div className="card-container">
-        <div className="card">
-            <div className="card-body">
-                <span className="card-number card-circle">1 </span>
-                <span className="card-author"> Breakfast </span>
-                <h2 className="card-title">Maggi </h2>
-                <span className="card-description">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam perspiciatis fugit quia dolores fuga. Nesciunt?
-                </span>
-                <div className="card-read">Read</div>
-
-            </div>
-            <img src={image} alt="images" className="card-media" />
-            <span className="card-tag">Order Now</span>
-
-        </div>
-      
-      </div>
+   
     </>
    
   )
