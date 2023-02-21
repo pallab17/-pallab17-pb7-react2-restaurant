6ecc1709ef66caa6461f80d71c58f8da3cf74d8e
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-22 00:14:23 +0530

    files updated

---
+++ pb7react2/src/components/basics/Restaurant.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./style.css";
+
+
+ const Restaurant = () => {
+  return (
+    <>
+     <div className="card-container">
+        <div className="card">
+            <div className="card-body">
+                <span className="card-number card-circle">1 </span>
+                <span className="card-author"> Breakfast </span>
+                <h2 className="card-title">Maggi </h2>
+                <span className="card-description">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam perspiciatis fugit quia dolores fuga. Nesciunt?
+                </span>
+                <div className="card-read">Read</div>
+
+            </div>
+            <img src={image} alt="images" className="card-media" />
+            <span className="card-tag">Order Now</span>
+
+        </div>
+      
+      </div>
+    </>
+   
+  )
+}
+
+export default Restaurant;
+
+
+
+    
