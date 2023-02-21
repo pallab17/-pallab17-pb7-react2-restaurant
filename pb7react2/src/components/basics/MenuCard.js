e1cacaf0c972218d592816e242f6d5c4fdd0e8a6
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-22 01:12:02 +0530

    files updated

--- pb7react2/src/components/basics/MenuCard.js
+++ pb7react2/src/components/basics/MenuCard.js
@@ -1,26 +1,39 @@
 import React from 'react'
 
-const MenuCard = () => {
+const MenuCard = ({menuData}) => {
   return (
     <>
-        <div className="card-container">
-        <div className="card">
-            <div className="card-body">
-                <span className="card-number card-circle">1 </span>
-                <span className="card-author"> Breakfast </span>
-                <h2 className="card-title">Maggi </h2>
-                <span className="card-description">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam perspiciatis fugit quia dolores fuga. Nesciunt?
-                </span>
-                <div className="card-read">Read</div>
+    <section className="main-card--cointainer">
+        {/* .map fn use kore amra basically for loop use korchi 
+        ekta key variable o use korte hoye to use map fn */}
+        {menuData.map((curElem) => {
+            return(
+                <>
+                <div className="card-container " key={curElem.id}>
+                <div className="card">
+                    <div className="card-body">
+                        <span className="card-number card-circle">{curElem.id}</span>
+                        <span className="card-author"> {curElem.name} </span>
+                        <h2 className="card-title">{curElem.name}</h2>
+                        <span className="card-description">
+                            {curElem.description}
+                        </span>
+                        <div className="card-read">Read</div>
+        
+                    </div>
+                    <img src={curElem.image} alt="images" className="card-media" />
+                    <span className="card-tag">Order Now</span>
+        
+                </div>
+              
+              </div>
+              </>
 
-            </div>
-            <img src={image} alt="images" className="card-media" />
-            <span className="card-tag">Order Now</span>
+            )
 
-        </div>
-      
-      </div>
+        })}
+    
+    </section>
     </>
   )
 }
--- pb7react2/src/components/basics/Restaurant.js
+++ pb7react2/src/components/basics/Restaurant.js
@@ -1,16 +1,37 @@
 import React,{useState} from 'react';
 import "./style.css";
 import Menu from "./menuApi";
+import MenuCard from './MenuCard.js';
+
 
 
  const Restaurant = () => {
-    const [menuData,setMenuData] = useState(Menu);
+    const [menuData,setMenuData] = useState(Menu); 
+   /*  usestate(menu) = usestate data manage kore 
+    menuapi.js theke data ta ese store hocche Menu bole use usestate e
+    usestate 2 to element pass kore array r form e
+    first element ta current state 
+    2nd element ta hooche updated/future state */
+
     console.log(menuData);
+
 
   return (
     <>
-   
+     
+    <MenuCard menuData={menuData} />
+    {/* eibar menucard.js file e jehetu ekbar component ta banano ache
+    10 bar use korar jonno 10 bar copy paste na korleo hobe
+    jate 10 bar na use korte hoye 
+    amra props use korbo i.e. prothome menuapi.js theke data ta menuData te store hobe
+    eibar amra menudata take props hisabe share korbo menucard.js e
+    tarpor map fn use kore loop kore debo jate automatically data ta show hoye browser e
+    
+      */}
     </>
+   
+
+    
    
   )
 }
